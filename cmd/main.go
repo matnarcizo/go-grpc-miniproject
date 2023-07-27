846f094ddccfa2b9104da3a1a5b7801cf5f7eb9d
Mateus Oliveira <mat.noliveira@gmail.com>
2023-07-26 23:59:57 -0300

    create greeting service and protobuffs

---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"go_grpc_miniproject/internal/svc"
+	greeting_v1 "go_grpc_miniproject/pkg/pb/greeting/v1"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/{{YOUR_REPO}}/internal/svc"
+	greeting_v1 "github.com/{{YOUR_REPO}}/pkg/pb/greeting/v1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+var (
+	listener net.Listener
+	server   *grpc.Server
+	logger   *zap.Logger
+)
+
+func main() {
+	logger, _ = zap.NewProduction()
+	defer logger.Sync()
+
+	initListener()
+
+	server = grpc.NewServer()
+
+	greeting_v1.RegisterGreeterServiceServer(server, &svc.GreeterService{})
+	logger.Info("Handlers registered")
+
+	go signalsListener(server)
+
+	logger.Info("Starting gRPC server...")
+	if err := server.Serve(listener); err != nil {
+		logger.Panic("Failed to start gRPC server", zap.Error(err))
+	}
+}
+
+func initListener() {
+	var err error
+	address := "localhost:50051"
+
+	listener, err = net.Listen("tcp", address)
+	if err != nil {
+		logger.Panic("Failed to listen", zap.String("address", address), zap.Error(err))
+	}
+
+	logger.Info("Started listening...", zap.String("address", address))
+
+	return
+}
+
+func signalsListener(server *grpc.Server) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGNIT)
+	_ = <-sigs
+
+	logger.Info("Gracefully stopping server...")
+	server.GracefulStop()
+}
---
+++ internal/svc/handlers.go
@@ -0,0 +1,27 @@
+package svc
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/go_grpc_miniproject/pkg/pb/greeting/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type GreeterService struct{}
+
+func (gs GreeterService) Greet(ctx context.Context, req *pb.GreetRequest) (res *pb.GreetResponse, err error) {
+	if req.Msg == nil {
+		err = status.New(codes.InvalidArgument, "Message cannot be empty").Err()
+		return
+	}
+
+	helloMsg := fmt.Sprintf("%s, %s", req.Msg.Greeting.String(), req.Msg.Name)
+
+	res = &pb.GreetResponse{
+		Resp: helloMsg,
+	}
+
+	return
+}
